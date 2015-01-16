eb502db5b8eebdc07f4830072e0a0fb3afcd17e5
Steven Maasch <steven.maasch@gmx.de>
2015-01-16 21:05:59 +0100

    Refactor

--- apt-batch-install.pl
+++ apt-batch-install.pl
@@ -9,8 +9,7 @@ use Text::CSV;
 use 5.18.0;
 
 my $script = basename $0;
-my $script_version = "1.0";
-
+my $script_version = "1.1";
 my $sep = "-" x 80;
 
 my $usage = <<EOQ;
@@ -23,11 +22,11 @@ Optional arguments:
 		--exclude	seperated package names to exclude
 EOQ
 
-my $verion = "";
 my $help = "";
-
 my $csv_file = "";
 my @exclude = "";
+
+&main();
 
 sub main {
 	my $ok = GetOptions(
@@ -39,19 +38,18 @@ sub main {
 		print $usage;
 		exit;
 	}
-	
-	say "Start installing packages from $csv_file";
-	say "Found " . &count_lines(file=>$csv_file) . " package(s) in file";
-	
-	if (@exclude) {
-		say "Exclude " . scalar @exclude . " package(s) => @exclude";
-	}
 
+	say "Package file => $csv_file";
+	say "Found " . &count_lines(file=>$csv_file) . " package(s) in file";
+	say "Exclude " . scalar @exclude . " package(s) => @exclude" if @exclude;
+	say "Start installing packages";
+	say $sep;
+	
 	my @packages;
 	my $csv = Text::CSV->new({ binary => 1 }) or 
-		die "Can't use CSV: " . Text::CSV->error_diag();
+		die "Can't use Text::CSV: " . Text::CSV->error_diag();
 
-	open my $fh, "<:encoding(utf8)", "test.csv" or die "test.csv: $!";
+	open my $fh, "<:encoding(utf8)", $csv_file or die "$csv_file: $!";
 	while (my $row = $csv->getline($fh)) {
 		my $package_name = $row->[0];
 		my $package_repo = $row->[1];
@@ -87,38 +85,41 @@ sub install_packages() {
 	for (@$packages) {
 		my $p_name = $_->{name};
 		my $p_repo = $_->{repo};
-		
+
 		say "Processing package: $p_name";
 		say $sep;
 
 		my $err = 0;
 		
 		if ($p_repo) {
+			# Add repo
 			system("add-apt-repository", "-y", $p_repo) == 0 or $err = 1;
-			system("apt-get", "update") unless ($err);
-			$err = 1 if ($?);
+			if (!$err) {
+				# Update package index
+				system("apt-get", "update") == 0 or $err = 1;	
+			}
 		}
-		system("apt-get", "install", "-y", $p_name) unless $err;
-		$err = 1 if ($?);
-		
+
+		if (!$err) {
+			# Install package
+			system("apt-get", "install", "-y", $p_name) == 0 or $err = 1;
+		}
+
 		if ($err) {
 			push(@err_installed, $p_name);
 		}
-		
 		say $sep;
 	}
 	say "Complete!";
-	
+
 	my $num_total = @$packages;
 	my $num_installed = $num_total - @err_installed;
 	my $num_not_installed = @err_installed;
 	say "Summery:";
-	say "\tTotal: $num_total package(s)";
-	say "\tInstalled: $num_installed package(s)";
+	say "Total => $num_total package(s)";
+	say "Installed => $num_installed package(s)";
+
 	if (@err_installed) {
-		say "\tNot installed: $num_not_installed packages(s) => @err_installed";
+		say "Not installed  $num_not_installed packages(s) => @err_installed";
 	}
 }
-
-
-&main();
